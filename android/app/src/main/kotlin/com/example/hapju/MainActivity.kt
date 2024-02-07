e315c052c53bf1207a4958eec8cf36518c676541
bakhwee-bug <parksy8103@gmail.com>
2024-02-07 19:03:05 +0900

    siyun push2

---
+++ android/app/src/main/kotlin/com/example/hapju/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hapju
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/const/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Colors.white;
+const primaryColor = Color(0xff8247FF);
+const secondaryColor = Color(0xffF2F3F9);
